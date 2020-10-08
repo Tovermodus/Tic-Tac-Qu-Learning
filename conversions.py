999de7070934332b3878d5e81eb85b02dfcc0df5
nvemonds <72505336+nvemonds@users.noreply.github.com>
2020-10-08 20:03:18 +0200

    Update conversions.py

--- conversions.py
+++ conversions.py
@@ -1,9 +1,29 @@
 
-def gameBoardStatusToLearningBoardStatus(gameBoardStatus):
-    print()
-
-def gameResultToLearningReward(gameResult):
-    print()
+def gameResultToLearningReward(allowed, victory, playerID):
+    """ 0=game not ended yet
+        1= x wins
+        2= o wins
+        3=draw"""
+        
+    if allowed:
+        if (victory==1 and playerID==0) or (victory==2 and playerID==1):
+            return 300
+        elif (victory==2 and playerID==0) or (victory==1 and playerID==1):
+            return -100        
+    else:
+        return -500
 
 def learningActionToGameAction(learningAction):
-    print()
+    actions=[0]*45
+    counter=0
+    for l in range(9):
+            for j in range(l):
+                actions[counter]=([j,l],2)
+                counter+=1
+    for l in range(9):
+        actions[counter]=([l],1)
+        counter+=1
+    return actions
+                
+actions=learningActionToGameAction(3)
+#%%
