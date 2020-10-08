3eafe22b3b2ad60bfa3bae77f7c9d56502f642e1
Simon Schmidt <simon.schmidt@mabisile.de>
2020-10-08 13:37:06 +0200

    Started implementation of QLearner

---
+++ Interface.py
@@ -0,0 +1,19 @@
+import game.game
+import learning.qLearning
+
+players = []
+actions = 30
+
+
+def getReward(playerId, actionID):
+    print()
+
+
+def getAction(playerId, state):
+    print()
+
+def getCurrentState():
+    print()
+
+def getStateAfterAction(playerId, actionID):
+    print()
