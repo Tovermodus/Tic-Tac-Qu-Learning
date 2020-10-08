4bae8941ecb0b63a2f51cf6be13b16f54a99ed1e
Leo Walz <leo@Leos-MacBook-Air.local>
2020-10-08 11:30:14 +0200

    Test

--- game/game.py
+++ game/game.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-print('hi')
+from qiskit import *
+from math import pi
+import numpy as np
+from qiskit.visualization import plot_bloch_multivector, plot_histogram
+from qiskit.tools.visualization import circuit_drawer
+from qiskit.visualization import plot_histogram
+
+qc = QuantumCircuit(3)
+# Apply H-gate to each qubit:
+for qubit in range(3):
+    qc.h(qubit)
+# See the circuit:
+qc.draw()
+circuit_drawer(qc)
+
+qc.show()
+print('finished')
