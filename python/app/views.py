25fc0aa76fd02722fcd4287009c4a9c3d418da67
Simon Schmidt <simon.schmidt@mabisile.de>
2021-10-01 18:26:38 +0200

    Added superposition error handling functionality

--- python/app/views.py
+++ python/app/views.py
@@ -1,8 +1,9 @@
 from app import app, db, Game, games_schema, Move, moves_schema
-from flask import request, jsonify
+from flask import jsonify
 import datetime
 
-def make_error(status_code, message, details = None):
+
+def make_error(status_code, message, details=None):
     response = jsonify({
         'status': status_code,
         'message': message,
@@ -11,9 +12,11 @@ def make_error(status_code, message, details = None):
     response.status_code = status_code
     return response
 
+
 @app.route('/')
 def helloWorld():
     return "Hello World"
+
 
 @app.route("/singleplayer/new/<name>/<player1>")
 def singleplayer_create_game(name, player1):
@@ -24,6 +27,7 @@ def singleplayer_create_game(name, player1):
     db.session.commit()
     games = Game.query.filter(Game.name == name).all()
     return jsonify(games_schema.dump(games))
+
 
 def count_occupation(gameID, position):
     moves_in_game = Move.query.filter(Move.game_id == gameID)
@@ -39,18 +43,42 @@ def count_occupation(gameID, position):
                 occupation += 0.5
     return occupation
 
+
 @app.route("/move/simple/<name>/<player>/<position>")
 def insert_simple_move(name, player, position):
     position = int(position)
-    if position < 0 or position > 8:
+    if position < 1 or position > 9:
         return make_error(412, "position does not exist")
     game = Game.query.filter(Game.name == name).first()
     if game is None:
         return make_error(412, "name of game does not exist in database")
+    if player not in [game.player1, game.player2]:
+        return make_error(412, "name of player does not belong to game")
     gameID = game.id
     if count_occupation(gameID, position) > 0:
         return make_error(412, "position already occupied")
     new_move = Move(position=position, type=1, player=player, game_id=gameID)
+    db.session.add(new_move)
+    db.session.commit()
+    all_moves_for_game = Move.query.filter(Move.game_id == gameID).all()
+    return jsonify(moves_schema.dump(all_moves_for_game))
+
+
+@app.route("/move/superposition/<name>/<player>/<position1>/<position2>")
+def insert_superposition_move(name, player, position1, position2):
+    position1 = int(position1)
+    position2 = int(position2)
+    if position1 < 1 or position1 > 9 or position2 < 1 or position2 > 9:
+        return make_error(412, "position does not exist")
+    game = Game.query.filter(Game.name == name).first()
+    if game is None:
+        return make_error(412, "name of game does not exist in database")
+    if player not in [game.player1, game.player2]:
+        return make_error(412, "name of player does not belong to game")
+    gameID = game.id
+    if count_occupation(gameID, position1) > 0.5 or count_occupation(gameID, position2) > 0.5:
+        return make_error(412, "position already occupied")
+    new_move = Move(position=position1 * 10 + position2, type=2, player=player, game_id=gameID)
     db.session.add(new_move)
     db.session.commit()
     all_moves_for_game = Move.query.filter(Move.game_id == gameID).all()
--- python/tests/move.py
+++ python/tests/move.py
@@ -12,12 +12,24 @@ class MoveTest(unittest.TestCase):
             Game.query.delete()
             games_json = app.views.singleplayer_create_game("Spiel", "ich")
             games = json.loads(games_json.get_data())
-            moves_json = app.views.insert_simple_move("Spiel", "ich", "0")
+            moves_json = app.views.insert_simple_move("Spiel", "ich", "1")
             moves = json.loads(moves_json.get_data())
             gameID = games[-1]["id"]
             assert moves[-1]["game_id"] == gameID
             assert moves[-1]["type"] == 1
-            assert moves[-1]["position"] == 0
+            assert moves[-1]["position"] == 1
+
+    def test_simple_move_wrong_name(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_simple_move("Spiel", "du", "1")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "name of player does not belong to game"
+            assert moves["status"] == 412
 
     def test_simple_move_no_game(self):
         with app.app.app_context():
@@ -25,7 +37,7 @@ class MoveTest(unittest.TestCase):
             Game.query.delete()
             games_json = app.views.singleplayer_create_game("Spiel", "ich")
             games = json.loads(games_json.get_data())
-            moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "0")
+            moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "1")
             moves = json.loads(moves_json.get_data())
             gameID = games[-1]["id"]
             assert moves["message"] == "name of game does not exist in database"
@@ -37,7 +49,7 @@ class MoveTest(unittest.TestCase):
             Game.query.delete()
             games_json = app.views.singleplayer_create_game("Spiel", "ich")
             games = json.loads(games_json.get_data())
-            moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "9")
+            moves_json = app.views.insert_simple_move("Spiel", "ich", "0")
             moves = json.loads(moves_json.get_data())
             gameID = games[-1]["id"]
             assert moves["message"] == "position does not exist"
@@ -56,14 +68,120 @@ class MoveTest(unittest.TestCase):
             assert moves["message"] == "position already occupied"
             assert moves["status"] == 412
 
-    # def test_superposition_move(self):
-    #     with app.app.app_context():
-    #         Game.query.delete()
-    #         ret = app.views.singleplayer_create_game("Spiel3", "ich")
-    #         result = json.loads(ret.get_data())
-    #         assert result[-1]["name"] == "Spiel3"
-    #         assert result[-1]["player1"] == "ich"
-    #         assert result[-1]["player2"] is None
+    def test_superposition_move(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "1", "2")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves[-1]["game_id"] == gameID
+            assert moves[-1]["type"] == 2
+            assert moves[-1]["position"] == 12
+
+    def test_superposition_move_wrong_name(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_superposition_move("Spiel", "du", "1","3")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "name of player does not belong to game"
+            assert moves["status"] == 412
+
+    def test_superposition_move_no_game(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_superposition_move("Anderes Spiel", "ich", "1","2")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "name of game does not exist in database"
+            assert moves["status"] == 412
+
+    def test_superposition_move_bad_position1(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "0","3")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position does not exist"
+            assert moves["status"] == 412
+
+    def test_superposition_move_bad_position2(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "4","10")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position does not exist"
+            assert moves["status"] == 412
+
+    def test_superposition_move_same_position1(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            app.views.insert_superposition_move("Spiel", "ich", "4","5")
+            app.views.insert_superposition_move("Spiel", "ich", "4","5")
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "4","6")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position already occupied"
+            assert moves["status"] == 412
+
+    def test_superposition_move_same_position2(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            app.views.insert_superposition_move("Spiel", "ich", "4","5")
+            app.views.insert_superposition_move("Spiel", "ich", "4","5")
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "1","4")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position already occupied"
+            assert moves["status"] == 412
+
+            def test_superposition_move_same_position3(self):
+                with app.app.app_context():
+                    Move.query.delete()
+                    Game.query.delete()
+                    games_json = app.views.singleplayer_create_game("Spiel", "ich")
+                    games = json.loads(games_json.get_data())
+                    app.views.insert_simple_move("Spiel", "ich", "1")
+                    moves_json = app.views.insert_superposition_move("Spiel", "ich", "1", "4")
+                    moves = json.loads(moves_json.get_data())
+                    gameID = games[-1]["id"]
+                    assert moves["message"] == "position already occupied"
+                    assert moves["status"] == 412
+
+    def test_superposition_move_same_position4(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            app.views.insert_simple_move("Spiel", "ich", "4")
+            moves_json = app.views.insert_superposition_move("Spiel", "ich", "1","4")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position already occupied"
+            assert moves["status"] == 412
 
 
 if __name__ == '__main__':
