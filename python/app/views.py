af5ad4a4b73c0fbb90115fc3c521831445f61a17
Simon Schmidt <simon.schmidt@mabisile.de>
2021-10-01 16:54:18 +0200

    Added error handling for new simple move

--- python/app/views.py
+++ python/app/views.py
@@ -25,14 +25,32 @@ def singleplayer_create_game(name, player1):
     games = Game.query.filter(Game.name == name).all()
     return jsonify(games_schema.dump(games))
 
+def count_occupation(gameID, position):
+    moves_in_game = Move.query.filter(Move.game_id == gameID)
+    occupation = 0
+    for move in moves_in_game:
+        if move.type == 1:
+            if move.position == position:
+                occupation += 1
+        if move.type == 2:
+            if move.position // 10 == position:
+                occupation += 0.5
+            if move.position % 10 == position:
+                occupation += 0.5
+    return occupation
 
 @app.route("/move/simple/<name>/<player>/<position>")
 def insert_simple_move(name, player, position):
+    position = int(position)
+    if position < 0 or position > 8:
+        return make_error(412, "position does not exist")
     game = Game.query.filter(Game.name == name).first()
     if game is None:
         return make_error(412, "name of game does not exist in database")
     gameID = game.id
-    new_move = Move(position = int(position), type = 1, player = player, game_id=gameID)
+    if count_occupation(gameID, position) > 0:
+        return make_error(412, "position already occupied")
+    new_move = Move(position=position, type=1, player=player, game_id=gameID)
     db.session.add(new_move)
     db.session.commit()
     all_moves_for_game = Move.query.filter(Move.game_id == gameID).all()
--- python/tests/move.py
+++ python/tests/move.py
@@ -19,17 +19,42 @@ class MoveTest(unittest.TestCase):
             assert moves[-1]["type"] == 1
             assert moves[-1]["position"] == 0
 
-    # def test_simple_move_no_game(self):
-    #     with app.app.app_context():
-    #         Move.query.delete()
-    #         Game.query.delete()
-    #         games_json = app.views.singleplayer_create_game("Spiel", "ich")
-    #         games = json.loads(games_json.get_data())
-    #         moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "0")
-    #         moves = json.loads(moves_json.get_data())
-    #         gameID = games[-1]["id"]
-    #         assert result["message"] == "name of game does not exist in database"
-    #         assert result["status"] == 412
+    def test_simple_move_no_game(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "0")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "name of game does not exist in database"
+            assert moves["status"] == 412
+
+    def test_simple_move_bad_position(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            moves_json = app.views.insert_simple_move("Anderes Spiel", "ich", "9")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position does not exist"
+            assert moves["status"] == 412
+
+    def test_simple_move_same_position(self):
+        with app.app.app_context():
+            Move.query.delete()
+            Game.query.delete()
+            games_json = app.views.singleplayer_create_game("Spiel", "ich")
+            games = json.loads(games_json.get_data())
+            app.views.insert_simple_move("Spiel", "ich", "4")
+            moves_json = app.views.insert_simple_move("Spiel", "ich", "4")
+            moves = json.loads(moves_json.get_data())
+            gameID = games[-1]["id"]
+            assert moves["message"] == "position already occupied"
+            assert moves["status"] == 412
 
     # def test_superposition_move(self):
     #     with app.app.app_context():
